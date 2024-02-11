70f9c4a407082950de218e336fd81e999460dd9b
Unam Xhego <Unam.Xhego@sita.co.za>
2024-02-11 20:46:46 +0200

    first commit

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        void Add(T entity);
+        // void Update(T entity); logic might be different when updating a category than updating a product
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveAll(IEnumerable<T> entity);
+    }
+}
---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);   
+            if (objFromDb != null)
+            {
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Description = obj.Description;
+                objFromDb.Title = obj.Title;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                if(obj.ImageURL != null)
+                {
+                    objFromDb.ImageURL = obj.ImageURL;
+                }
+            }
+
+        }
+    }
+}
